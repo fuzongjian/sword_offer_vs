7b61db1c819d0ce34f44c4501ab9da8f68077c01
fuzongjian <fu_zongjian@163.com>
2020-05-09 16:28:55 +0800

    update

--- sevenSort.cpp
+++ sevenSort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 class Solution
@@ -89,9 +90,52 @@ public:
         }
     }
     static void shell_sort(int num[], int count){
+        int shellNum = 2;
+        int gap = round(count / shellNum);
+        while (gap > 0)
+        {
+            for (int i = gap; i < count; i++)
+            {
+                int temp = num[i];
+                int j = i;
+                while (j >= gap && num[j - gap] > temp)
+                {
+                    num[j] = num[j - gap];
+                    j = j - gap;
+                }
+                num[j] = temp;
+            }
+            gap = round(gap/shellNum);
+            Solution::print(num, count, "多次循环: ");
+        }
+    }
+    static void fast_sort(int num[], int left, int right){
+        if(left > right)return;
+        int i, j, base, temp;
+        i = left, j = right;
+        base = num[left]; // 取最左边的数为基准数
+        while (i < j)   
+        {
+            while (num[j] >= base && i < j)j--;
+            while (num[i] <= base && i < j)i ++;
+            if (i < j)  
+            {
+                temp = num[i];
+                num[i] = num[j];
+                num[j] = temp;
+            }
+        }
+        num[left] = num[i];
+        num[i] = base;
+        Solution::print(num, 8, "多次循环: ");
+        fast_sort(num, left, i - 1);// 递归左边
+        fast_sort(num, i + 1,right);// 递归右边
+    }
+    static void heap_sort(int num[], int count){
 
     }
 };
+
 int main(){
     // 七种常见的排序
     int array[] = {9,1,2,5,4,6,8,7};
@@ -106,8 +150,11 @@ int main(){
     // 二分插入排序
     // Solution::binaryInsert_sort(array, count);
     // 希尔排序
-    Solution::shell_sort(array, count);
+    // Solution::shell_sort(array, count);
+    // 快速排序
+    // Solution::fast_sort(array, 0, count-1);
+    Solution::heap_sort(array, count);
 
-    Solution::print(array, count, "二分插入排序: ");
+    Solution::print(array, count, "快速排序: ");
     return 0;
 }
